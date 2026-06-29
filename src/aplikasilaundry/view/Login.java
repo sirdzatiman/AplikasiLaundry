63c734ec5ca1c6c7b8f1780326019fb067b62af0
betrindina <betrindina@gmail.com>
2026-06-29 15:52:04 +0700

    bismillah

--- src/aplikasilaundry/view/Login.java
+++ src/aplikasilaundry/view/Login.java
@@ -25,7 +25,7 @@ public class Login extends javax.swing.JFrame {
      */
       public Login() {
         initComponents();
-        
+       
         
         panelLengkung(panelUser);
         panelLengkung(jPanel56);
