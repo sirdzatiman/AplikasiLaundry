5b47f039d9f475094cd50f7595e5506d19ad695c
betrindina <betrindina@gmail.com>
2026-07-04 23:06:39 +0700

    pembenaran package1

--- src/aplikasilaundry/view/panel/popUpLogout.java
+++ src/aplikasilaundry/view/panel/popUpLogout.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
  */
-package aplikasilaundry.view.dialog;
+package aplikasilaundry.view.panel;
 
 import aplikasilaundry.view.frame.FrameLogin;
 
